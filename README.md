1de0e5fa12c0a7c4fed7a60d4b9aa9680557dc02
Subbu Devasani <56950679+SubbuDevasani@users.noreply.github.com>
2026-01-09 09:58:14 +0000

    test code

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# okta-deployments
+1. Update Package Repository
+    $ sudo apt upgrade
+
+2. Upgrade Existing Packages
+    $ sudo apt upgrade
+
+3. Install Python 3
+    $ sudo apt install python3
+
+4. Install Python Package Manager (pip)
+    $ sudo apt install python3-pip
+
+5. Install Additional Tools for Development
+    $ sudo apt install python3-dev python3-venv build-essential
+
+6. Export Dev env as below to fetch dev OKTA configs
+    $ export OKTA_API_TOKEN="00ZdcRd4znTik54Yiw3Es9vkCDe8IIGEwlVagnVd3i"
+    $ export OKTA_DOMAIN="demo-dev-deployments.okta.com"
+
+7. Execute Python script to fetch dev configs
+    $ python final_dev_till_auth_server.py
---
+++ terraform-okta/main.tf
@@ -0,0 +1,50 @@
+#################################
+#OIDC APPLICATIONS
+#################################
+module "oidc_apps" {
+  source = "./modules/oidc_app"
+
+  for_each = var.oidc_apps
+
+  label                     = each.value.label
+  application_type          = each.value.application_type
+  grant_types               = each.value.grant_types
+  response_types            = each.value.application_type == "service" ? [] : each.value.response_types
+
+  redirect_uris             = each.value.application_type == "service" ? [] : concat(each.value.redirect_uris_test, each.value.redirect_uris_prod)
+
+  post_logout_redirect_uris = each.value.application_type == "service"  ? []  : each.value.post_logout_redirect_uris
+  consent_method            = each.value.consent_method
+  issuer_mode               = each.value.issuer_mode
+  refresh_token_rotation    = each.value.refresh_token_rotation
+}
+
+####################################
+#SAML APPLICATIONS
+####################################
+module "saml_apps" {
+  source = "./modules/saml_app"
+
+  for_each = var.saml_apps
+
+  label                    = each.value.label
+  sso_acs_url              = each.value.sso_acs_url
+  recipient                = each.value.recipient
+  destination              = each.value.destination
+  audience                 = each.value.audience
+  idp_issuer               = each.value.idp_issuer
+
+  subject_name_id_template = each.value.subject_name_id_template
+  subject_name_id_format   = each.value.subject_name_id_format
+
+  response_signed          = each.value.response_signed
+  assertion_signed         = each.value.assertion_signed
+
+  signature_algorithm      = each.value.signature_algorithm
+  digest_algorithm         = each.value.digest_algorithm
+
+  honor_force_authn        = each.value.honor_force_authn
+  authn_context_class_ref  = each.value.authn_context_class_ref
+
+  attribute_statements     = each.value.attribute_statements
+}
